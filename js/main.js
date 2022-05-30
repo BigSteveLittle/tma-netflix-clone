fd420ce0c10c6fd8ec1410eb684ca709d6bda4af
BigSteveLittle <bigsteve@redandblackzone.com>
2022-05-30 18:32:00 +1000

    adds final corrections to main.js
    1/ Adds '-item' to query selector '.tab-content-item' of 'tabContentItems'.
    This resolves issue of all tab contents staying visible.
    2/ Adds ';' to forEach in 'removeBorder' and 'removeShow' functions.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 // Invoke strict mode.
 "use strict";
 // TAB SELECTION.
-console.log('beetroot2');
 // 'tab-item': grab the 3 tab selectors from the DOM.
 const tabItems = document.querySelectorAll('.tab-item');
 // 'tab-content': grab the tab content from the DOM.
-const tabContentItems = document.querySelectorAll('.tab-content');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
 // 'selectItem': function to display the tab content according to what tab is clicked.
 function selectItem(e) {
     // Remove current tab border-bottom.
@@ -20,10 +19,10 @@ function selectItem(e) {
     tabContentItem.classList.add('show');
 }
 function removeBorder() {
-    tabItems.forEach(item => item.classList.remove('tab-border'))
+    tabItems.forEach(item => item.classList.remove('tab-border'));
 }
 function removeShow() {
-    tabContentItems.forEach(item => item.classList.remove('show'))
+    tabContentItems.forEach(item => item.classList.remove('show'));
 }
 // 'tab-item': add an event listener for when a 'tab-item' is clicked.
 tabItems.forEach(item => item.addEventListener('click', selectItem));
